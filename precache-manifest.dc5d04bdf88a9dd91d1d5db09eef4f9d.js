3655d3e7a3008ae330c3dc65243fc8096bcdf0b8
Михаил Кормановский <kormanowsky@gmail.com>
2020-09-07 22:10:48 +0300

    Add files

---
+++ precache-manifest.dc5d04bdf88a9dd91d1d5db09eef4f9d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cf9e0b5f804b278c70a68bcdd08632",
+    "url": "/tutoria-built/index.html"
+  },
+  {
+    "revision": "24c5b4cf6eec0f8445b2",
+    "url": "/tutoria-built/static/css/2.6bad2236.chunk.css"
+  },
+  {
+    "revision": "da7b986ed7b98ef2e474",
+    "url": "/tutoria-built/static/css/main.471f326f.chunk.css"
+  },
+  {
+    "revision": "24c5b4cf6eec0f8445b2",
+    "url": "/tutoria-built/static/js/2.55a2ceb3.chunk.js"
+  },
+  {
+    "revision": "a1d77a2a5dce024c5a06d974cfedfbc9",
+    "url": "/tutoria-built/static/js/2.55a2ceb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da7b986ed7b98ef2e474",
+    "url": "/tutoria-built/static/js/main.5b7061b0.chunk.js"
+  },
+  {
+    "revision": "a5579d136877cfbc8353",
+    "url": "/tutoria-built/static/js/runtime-main.36e4ccf6.js"
+  },
+  {
+    "revision": "88932dadc42e1bba93b21a76de60ef7a",
+    "url": "/tutoria-built/static/media/MONTSERRAT-BOLD.88932dad.ttf"
+  },
+  {
+    "revision": "c8e2ff59468f36839a21751c3b310334",
+    "url": "/tutoria-built/static/media/MONTSERRAT-BOLD.c8e2ff59.woff"
+  },
+  {
+    "revision": "a98626e1aef6ceba5dfc1ee7112e235a",
+    "url": "/tutoria-built/static/media/MONTSERRAT-MEDIUM.a98626e1.ttf"
+  },
+  {
+    "revision": "c304ae695ccf2d63586702b89fa868ff",
+    "url": "/tutoria-built/static/media/MONTSERRAT-MEDIUM.c304ae69.woff"
+  }
+]);
